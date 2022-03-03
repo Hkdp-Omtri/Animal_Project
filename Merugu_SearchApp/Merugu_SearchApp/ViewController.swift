8059e972233c1e479caebd86f7e2d34804d5cc8b
Homakesavadurgaprasad <89258140+Hkdp-Omtri@users.noreply.github.com>
2022-03-03 11:02:39 -0600

    Add files via upload

--- Merugu_SearchApp/Merugu_SearchApp/ViewController.swift
+++ Merugu_SearchApp/Merugu_SearchApp/ViewController.swift
@@ -13,9 +13,9 @@ class ViewController: UIViewController {
     var emptySearchImage = [UIImage(named: "imagenotfound.jpg")]
     var wordEntered:String = ""
     
-    var car_keywords = ["Model", "indianbrand", "milegecar", "offroadingcar", "formulacar"]
-    var flower_keywords = ["beautifulplant", "smellable", "seedplant", "winterplant" , "springflower"]
-    var animals_keywords = ["cuteanimal", "water horse", "jungleking", "buck", "trunk"]
+    var car_keywords = ["audi", "tata", "honda", "mahindra", "lamborghini"]
+    var flower_keywords = ["rose", "jasmin", "sunflower", "tulips" , "dafodil"]
+    var animals_keywords = ["panda", "hippo", "lion", "deer", "elephant"]
     
     let car_keywords_description = [ ["Audi is a German automative manufacture of luxury cars headquartered in Germany, founded by engineer August Horch"],["Tata cars is an Indian Car Manufacturer established in 1945, headquarted in Mumbai, founder J.R.D.Tata"],["Honda is Japanese Multinational company headquarted in Japan, founded by Hamamaatsu in 1948, its one of the leading cat makers in Japan"], ["Mahindra & Mahindra is Indian Multinational Automative Manufcturing Co-operation head quartered in Mumbai established in 1945 and its on of the fortune 500 company"], ["Automobili Lambo is an Italian brand and manufactureer of luxury sports car and company owned by volkswagon group, founded in 1963 by Ferruccio"]]
     
@@ -24,11 +24,14 @@ class ViewController: UIViewController {
     let animals_keywords_description = [ ["The Species belongs to China, it is characterized by bold black and white coat and rotund body, the lifespan is about 20 years and it belongs to herbivorouds"], ["Hippo species belongs to Saharan-Africa, its an Omnivorous animal and lifespan is 40 to 50 years, the max speed is 30 kmph"], ["Lion is a large cat of the genus Panthera native to Africa and India. It has mascular body  and king of jungle, it is a Carnivorus animal and lifespan is 10 t0 15 years"], ["Deer are hoofed ruminant mammals forming the family Cervidae. The two main groups are Cervinae, including th muntjac, the elk, the male deer is called buck"], ["Elephant are the largest existing land animals, Three living species are currently recognised: the african bush elephant, the african forest elephant and the Asian elephant, the lifespan is aroung 40 years"]]
     
 
+ 
     @IBOutlet weak var searchTextField: UITextField!
     
     
     
+    
     @IBOutlet weak var resultImage: UIImageView!
+    
     
     
     @IBOutlet weak var topicInfoText: UITextView!
@@ -45,13 +48,19 @@ class ViewController: UIViewController {
 
     
     
+    
     @IBAction func searchButtonAction(_ sender: UIButton) {
     }
+    
     
     @IBAction func showNextImageBtn(_ sender: UIButton) {
     }
     
-    @IBAction func showPrevImageBtn(_ sender: Any) {
+  
+    @IBAction func showPrevImageBtn(_ sender: UIButton) {
+    }
+    
+    @IBAction func ResetBtn(_ sender: UIButton) {
     }
 }
 
